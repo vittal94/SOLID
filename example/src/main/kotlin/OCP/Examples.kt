9a891760ab66d35dabde2f30fb9d75f0cefa6e39
vittal94 <evg94z@mail.ru>
2024-12-01 21:58:13 +0300

    gitrebase

---
+++ example/src/main/kotlin/OCP/Examples.kt
@@ -0,0 +1,71 @@
+package org.example.OCP
+
+//This class violates the OCP, because if we want to  add new type of Sender, we need to modify
+//Notifier class also
+object BadDesignOfSender {
+    class SMSSender{
+        fun sendSMS() {
+            println("Sending SMS.")
+        }
+    }
+
+    class PushSender {
+        fun sendPush() {
+            println("Sending push message.")
+        }
+    }
+
+    class EmailSender{
+        fun sendEmail(){
+            println("Sending email message.")
+        }
+    }
+
+    class Notifier(private val sender: Any) {
+        fun notify() {
+            if (sender is EmailSender) {
+                sender.sendEmail()
+            }
+            else if(sender is PushSender) {
+                sender.sendPush()
+            }
+            else if (sender is SMSSender) {
+                sender.sendSMS()
+            }
+            // if we add a new type of sender like DoveSender, we need to modify this class
+            //thus we violate the OCP because class should be close for modification
+            //else if(sender is DoveSender)
+            //    sender.sendByDove()
+        }
+    }
+}
+
+//if we use the abstraction of a Sender, we can extend the functionality of Notifier without modifying it
+//we modify just Sender
+object GoodDesignOfSender {
+    interface Sender {
+        fun send()
+    }
+    class SMSSender : Sender {
+        override fun send() {
+            println("Sending SMS.")
+        }
+    }
+    class EmailSender : Sender {
+        override fun send() {
+            println("Sending the email.")
+        }
+    }
+
+    class PushSender : Sender {
+        override fun send() {
+            println("Sending the push message.")
+        }
+    }
+
+    class Notifier(private val sender: Sender){
+        fun notify() {
+            sender.send()
+        }
+    }
+}
